577b7e200d9bfda6d3b5a95557fc66675bc2c37f
be3short <beshort@ucsc.edu>
2017-10-27 09:07:50 -0700

    Added file format classes

---
+++ src/main/java/com/be3short/io/format/FileFormat.java
@@ -0,0 +1,9 @@
+package com.be3short.io.format;
+
+public interface FileFormat
+{
+
+	public String extension();
+
+	public String label();
+}
---
+++ src/main/java/com/be3short/io/format/ImageFormat.java
@@ -0,0 +1,85 @@
+package com.be3short.io.format;
+
+public enum ImageFormat implements FileFormat
+{
+	PNG(
+		"PNG",
+		".png",
+		true,
+		true),
+	JPEG(
+		"JPEG",
+		".jpg",
+		true,
+		true),
+	GIF(
+		"gif",
+		".gif",
+		true,
+		true),
+	BMP(
+		"BMP",
+		".bmp",
+		true,
+		true),
+	SVG(
+		"SVG",
+		".svg",
+		false),
+	EPS(
+		"EPS",
+		".eps",
+		false);
+
+	public final String extension;
+	public final String name;
+	public final boolean image;
+	public final boolean needsBackground;
+
+	private ImageFormat(String name, String extension, boolean image)
+	{
+		this.name = name;
+		this.image = image;
+		this.needsBackground = false;
+		this.extension = extension;
+	}
+
+	private ImageFormat(String name, String extension, boolean image, boolean background)
+	{
+		this.name = name;
+		this.image = image;
+		this.needsBackground = background;
+		this.extension = extension;
+	}
+
+	public static ImageFormat getMatch(String name)
+	{
+		ImageFormat match = null;
+		for (ImageFormat format : ImageFormat.values())
+		{
+			if (format.name.toLowerCase().contains(name.toLowerCase()))
+			{
+				match = format;
+				break;
+			}
+		}
+		return match;
+	}
+
+	@Override
+	public String extension()
+	{
+		return extension;
+	}
+
+	@Override
+	public String label()
+	{
+		return name;
+	}
+
+	public FileSpecifications<ImageFormat> createFileSpecs(String file)
+	{
+		return new FileSpecifications<ImageFormat>(file, this);
+	}
+}
