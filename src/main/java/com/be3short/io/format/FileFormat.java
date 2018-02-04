3b01a585a6a3d7235d9773f1c00ef4e95e381b47
be3short <beshort@ucsc.edu>
2018-02-04 05:26:01 -0800

    Fixed bugs

--- src/main/java/com/be3short/io/format/FileFormat.java
+++ src/main/java/com/be3short/io/format/FileFormat.java
@@ -1,9 +1,127 @@
 package com.be3short.io.format;
 
+import java.io.File;
+
 public interface FileFormat
 {
 
 	public String getFileExtension();
 
 	public String getFormatName();
+
+	public <F extends FileFormat> F getDefaultFormat();
+
+	public static <F extends FileFormat> boolean isFileFormat(File file, F format)
+	{
+		Integer extensionLength = format.getFileExtension().length();
+
+		if (file.getName().length() > extensionLength)
+		{
+			if (file.getName().substring(extensionLength - 1).contains(format.getFileExtension()))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Checks if a file name is appended with the corresponding extension
+	 * 
+	 * @param <T>
+	 * 
+	 * @param file
+	 *            location of file to check
+	 * @param extension
+	 *            extension label of file type
+	 * @return true if the file contains the extension, and false otherwise
+	 *
+	 */
+	public static <T extends FileFormat> T checkFormat(File file)
+	{
+		T matching = null;
+		if (matching == null)
+		{
+			checkFormat(file);
+		}
+		T[] allFormats = (T[]) matching.getClass().getEnumConstants();
+		for (T fileFormat : allFormats)
+			try
+			{
+				Integer extLength = fileFormat.getFileExtension().length();
+				if (file.getName().length() > extLength)
+				{
+					if (file.getName().substring(extLength).contains(fileFormat.getFileExtension()))
+					{
+						matching = fileFormat;
+					}
+				}
+			} catch (Exception badFile)
+			{
+				System.err.println("Unable to determine what format the file " + file + " was");
+
+			}
+		return matching;
+
+	}
+
+	/**
+	 * Checks if a file name is appended with the corresponding extension
+	 * 
+	 * @param file
+	 *            location of file to check
+	 * @param extension
+	 *            extension label of file type
+	 * @return true if the file contains the extension, and false otherwise
+	 *
+	 */
+	public static boolean hasExtensionAppended(File file, String extension)
+	{
+		try
+		{
+			if (file.getName().length() > extension.length())
+			{
+				if (file.getName().contains(extension))
+				{
+					return true;
+				}
+			}
+		} catch (Exception badFile)
+		{
+			System.err.println("Unable to determine if file " + file + " format was " + extension);
+		}
+		return false;
+	}
+
+	/**
+	 * Adds the necessary extension to a file if it has not already been
+	 * included.
+	 * 
+	 * @param file
+	 *            location of file to check
+	 * @param extension
+	 *            extension label of file type
+	 * @return the file with the appended extension
+	 *
+	 */
+	public static File getExtensionAppended(File file, String extension)
+	{
+		File adjustedFile = file;
+		try
+		{
+
+			Boolean needsExtension = hasExtensionAppended(file, extension);
+			if (needsExtension)
+			{
+				adjustedFile = new File(file.getAbsoluteFile() + extension);
+			}
+		} catch (
+
+		Exception badFile)
+		{
+			System.out.println("Unable to determine if file " + file + " contained extension " + extension);
+		}
+		return adjustedFile;
+
+	}
 }
--- src/main/java/com/be3short/io/format/ImageFormat.java
+++ src/main/java/com/be3short/io/format/ImageFormat.java
@@ -1,35 +1,8 @@
 package com.be3short.io.format;
 
-public enum ImageFormat implements FileFormat
+public<F extends FileFormat>ImageFormat implements FileFormat{{public static enum ImgFormat}
 {
-	PNG(
-		"PNG",
-		".png",
-		true,
-		true),
-	JPEG(
-		"JPEG",
-		".jpg",
-		true,
-		true),
-	GIF(
-		"gif",
-		".gif",
-		true,
-		true),
-	BMP(
-		"BMP",
-		".bmp",
-		true,
-		true),
-	SVG(
-		"SVG",
-		".svg",
-		false),
-	EPS(
-		"EPS",
-		".eps",
-		false);
+	PNG("PNG",".png",true,true),JPEG("JPEG",".jpg",true,true),GIF("gif",".gif",true,true),BMP("BMP",".bmp",true,true),SVG("SVG",".svg",false),EPS("EPS",".eps",false);
 
 	public final String extension;
 	public final String name;
@@ -50,6 +23,8 @@ public enum ImageFormat implements FileFormat
 		this.image = image;
 		this.needsBackground = background;
 		this.extension = extension;
+	}
+
 	}
 
 	public static ImageFormat getMatch(String name)
@@ -82,4 +57,33 @@ public enum ImageFormat implements FileFormat
 	{
 		return new FileSpecifications<ImageFormat>(file, this);
 	}
+
+	@Override
+	public <F extends FileFormat> F getDefaultFormat()
+	{
+		// TODO Auto-generated method stub
+		return ImageFormat.EPS;
+	}
+
 }
+
+	@Override
+	public String getFileExtension()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getFormatName()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <F extends FileFormat> F getDefaultFormat()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
